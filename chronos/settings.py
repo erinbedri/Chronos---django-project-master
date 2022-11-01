8f778776983c8deab5550c22e3f6ca6737a9353f
erinbedri <erinbedri@gmail.com>
2022-11-01 12:52:08 +0100

    Moved static files into chronos project dir

--- chronos/settings.py
+++ chronos/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'chronos/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
