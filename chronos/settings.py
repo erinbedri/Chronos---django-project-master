29fbdcfc77002cba06f0b81fb020d59563ce8030
erinbedri <erinbedri@gmail.com>
2022-11-01 11:44:36 +0100

    Fixed static_url on settings

--- chronos/settings.py
+++ chronos/settings.py
@@ -121,15 +121,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFiles'
 
 MEDIA_URL = "media/"
 
