7f41da77737f69368d2159f5677c80795db89f75
erinbedri <erinbedri@gmail.com>
2022-11-01 11:33:02 +0100

    Installed whitenoise for static

--- chronos/settings.py
+++ chronos/settings.py
@@ -39,6 +39,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 MIDDLEWARE = [
     'chronos.web.middlewares.measure_time_middleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "media/"
 
